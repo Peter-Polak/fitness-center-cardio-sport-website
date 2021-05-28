666b7b65ae260d4b11ff77a74340d643a07bb790
Peter Polák <peter.polak.mail@gmail.com>
2021-05-28 02:00:27 +0200

    Add button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,95 @@
+import { Component } from "react";
+import styled from "styled-components";
+
+export enum ButtonType
+{
+    TRANSPARENT, NORMAL, CONFIRM, CANCEL
+}
+
+interface IButtonProps
+{
+    type : ButtonType
+    onClick : (event : any) => void
+    disabled? : boolean
+    className? : string
+}
+
+interface IButtonState
+{
+    
+}
+
+class Button extends Component<IButtonProps, IButtonState>
+{
+    constructor(props : IButtonProps)
+    {
+        super(props);
+        this.state = 
+        {
+            
+        }
+    }
+    
+    getButtonComponent(type : ButtonType)
+    {
+        switch(type)
+        {
+            case ButtonType.TRANSPARENT:
+                return TransparentButton;
+            case ButtonType.NORMAL:
+                return TextButton;
+            case ButtonType.CONFIRM:
+                return ConfirmButton;
+            case ButtonType.CANCEL:
+                return CancelButton;
+        }
+    }
+
+    render() : JSX.Element
+    {
+        const { type, onClick, disabled, className, children } = this.props;
+        const StyledButton = this.getButtonComponent(type);
+        
+        return (
+            <StyledButton onClick={onClick} disabled={disabled} className={className}>
+                {children}
+            </StyledButton>
+        );
+    }
+}
+
+const TransparentButton = styled.button`
+    padding: 0;
+    margin: 0;
+
+    color: ${props => props.theme.color.primary.normal};
+    background-color: transparent;
+    
+    &:disabled
+    {
+        opacity: 25%;
+        cursor: not-allowed;
+    }
+`;
+
+const TextButton = styled(TransparentButton)`
+    padding: 10px 40px;
+    
+    color: ${props => props.theme.color.background};
+    font-family: "Bebas Neue";
+    font-size: 24px;
+    letter-spacing: 1px;
+    background-color: ${props => props.theme.color.primary.normal};
+`;
+
+const ConfirmButton = styled(TextButton)`
+    color: ${props => props.theme.color.text};
+    background-color: ${props => props.theme.color.succes.normal};
+`;
+
+const CancelButton = styled(TextButton)`
+    color: ${props => props.theme.color.text};
+    background-color: ${props => props.theme.color.warning.normal};
+`;
+
+export default Button;
