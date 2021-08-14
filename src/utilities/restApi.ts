221ce5a38ed047ec95c4bb94d1446c7a6f26ddab
Peter Polák <peter.polak.mail@gmail.com>
2021-08-14 04:13:34 +0200

    Refactor code and add session overview table URL

--- src/utilities/restApi.ts
+++ src/utilities/restApi.ts
@@ -1,17 +1,10 @@
+import { settings } from "./settings";
 import { ReservationFormValidity, OrganizedSessions, IReservationForm, SessionsError, Reason } from "./types";
-
-export const backendServerUrls = 
-{
-    release : "https://script.google.com/macros/s/AKfycbzlOl57hBNV5fF8g_waOrjOscdaQm4oisdXID2n0hPI7-yjYBrEKZGQ333zZpYUyskIBw/exec",
-    dev : "https://script.google.com/macros/s/AKfycbxpAWLK9K4q22SEUAa3Ei45AsEE3zAtnH_b8B2W-dDDbP5kbPOwO_oTeoyHqt9YaWVzpw/exec"
-}
-
-export const currentServer = backendServerUrls.dev;
 
 export async function getSessions() : Promise<OrganizedSessions | Reason<OrganizedSessions, SessionsError>>
 {
     const response = await fetch(
-        currentServer,
+        settings.backendUrl,
         {
             method: 'GET'
         }
@@ -27,7 +20,7 @@ export async function getSessions() : Promise<OrganizedSessions | Reason<Organiz
 export async function postReservation(reservation : IReservationForm) : Promise<ReservationFormValidity>
 {
     const query = `?timestamp=${reservation.timestamp}&name=${reservation.name}&surname=${reservation.surname}&emailAddress=${reservation.emailAddress}&sessionsString=${reservation.sessionsString}`;
-    const url = currentServer + query;
+    const url = settings.backendUrl + query;
     
     const response = await fetch(
         url,
---
+++ src/utilities/settings.ts
@@ -0,0 +1,19 @@
+interface ApplicationSettings
+{
+    backendUrl : string
+    sessionOverviewTableUrl : string
+}
+
+const development : ApplicationSettings = 
+{
+    backendUrl : "https://script.google.com/macros/s/AKfycbxpAWLK9K4q22SEUAa3Ei45AsEE3zAtnH_b8B2W-dDDbP5kbPOwO_oTeoyHqt9YaWVzpw/exec",
+    sessionOverviewTableUrl : "https://docs.google.com/spreadsheets/d/e/2PACX-1vT2agEjJFrLSOqaRY0Sklzidr_-HeqHtcAQSLdziIGuSl152OBlls2uNZ9o9NCOlYRJJaAXX_f5fPHj/pubhtml?gid=513466788&amp;single=true&amp;widget=true&amp;headers=false",
+};
+
+const production : ApplicationSettings = 
+{
+    backendUrl : "https://script.google.com/macros/s/AKfycbzlOl57hBNV5fF8g_waOrjOscdaQm4oisdXID2n0hPI7-yjYBrEKZGQ333zZpYUyskIBw/exec",
+    sessionOverviewTableUrl : "https://docs.google.com/spreadsheets/d/e/2PACX-1vQLQhQqciBIdTlzJuguYshxnj2F4TgufIa5_xDAVIncSh7GLWSKqAh-FHaLv5wbvc1-JUW1--niaKWj/pubhtml?gid=984167338&amp;single=true&amp;widget=true&amp;headers=false",
+};
+
+export const settings = process.env.NODE_ENV === 'development' ? development : production;
