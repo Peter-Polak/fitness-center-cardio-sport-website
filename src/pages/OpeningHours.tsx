ab6641cc901b72fb661833c2edd5376158dcfcc3
Peter Polák <peter.polak.mail@gmail.com>
2022-01-11 21:54:47 +0100

    Update opening hours

--- src/pages/OpeningHours.tsx
+++ src/pages/OpeningHours.tsx
@@ -33,26 +33,15 @@ class OpeningHours extends Component<IOpeningHoursProps, IOpeningHoursState>
                 <Heading heading="H1"><MaterialIcon icon="schedule" color="dark"/> Otváracie hodiny</Heading>
                 
                 <Content>
-                    <p>Platné od 17.12.2021 do 09.01.2022</p>
-                    <Hours>
-                        <p>Pondelok: 15:00 - 20:00</p>
-                        <p>Utorok: 15:00 - 20:00</p>
-                        <p>Streda: 15:00 - 20:00</p>
-                        <p>Štvrtok: 15:00 - 20:00</p>
-                        <p>Piatok : 15:00 - 20:00</p>
-                        <p>Sobota: 15:00 - 20:00</p>
-                        <p>Nedeľa: 15:00 - 20:00</p>
-                    </Hours>
-
                     <p>Platné od 10.01.2022</p>
                     <Hours>
-                        <p>Pondelok: <Warning>ZATVORENÉ</Warning></p>
-                        <p>Utorok: <Warning>ZATVORENÉ</Warning></p>
-                        <p>Streda: <Warning>ZATVORENÉ</Warning></p>
-                        <p>Štvrtok: <Warning>ZATVORENÉ</Warning></p>
-                        <p>Piatok : <Warning>ZATVORENÉ</Warning></p>
-                        <p>Sobota: <Warning>ZATVORENÉ</Warning></p>
-                        <p>Nedeľa: <Warning>ZATVORENÉ</Warning></p>
+                        <p>Pondelok: 15:00 - 22:00</p>
+                        <p>Utorok: 15:00 - 16:00, 17:00 - 22:00</p>
+                        <p>Streda: 15:00 - 22:00</p>
+                        <p>Štvrtok: 15:00 - 16:00, 17:00 - 22:00</p>
+                        <p>Piatok : 15:00 - 22:00</p>
+                        <p>Sobota: 16:00 - 21:00</p>
+                        <p>Nedeľa: 16:00 - 21:00</p>
                     </Hours>
                     
                     <Warning>!!! UPOZORNENIE !!!</Warning>
